3817c818d9a8f45a4c70b84671ddda855bdbe75a
HeszMonika <heszmoni2001@gmail.com>
2020-09-16 07:32:17 +0100

    Add project files.

---
+++ Kiralyno/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiralyno
+{
+    class Tabla
+    {
+        char[,] T = new char[8, 8];
+        private char UresCella;
+        private char UresOszlopokSzama;
+        private char UresSorokSzama;
+
+        public void Elhelyez()
+        {
+
+        }
+
+        public void FajlbaIr()
+        {
+
+        }
+
+        public void Megjelenit()
+        {
+
+        }
+
+        public void Tabla()
+        {
+
+        }
+
+        public void UresOszlop()
+        {
+
+        }
+
+        public void UresSor()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
